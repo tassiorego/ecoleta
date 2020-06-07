d4f03debbf5b41f5ec89f58edff83354f4883802
Tassio Rego <tassiorego@gmail.com>
2020-06-07 09:59:18 -0300

    Adding default files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
+import { resolve } from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
