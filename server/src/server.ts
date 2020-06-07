5e531ce1779ffea267abe44e5b8453c15f4b7885
Tassio Rego <tassiorego@gmail.com>
2020-06-07 09:24:48 -0300

    refactoring routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request: Request, response: Response) => {
-  response.json({ message: 'Olá Mundo' });
-});
+app.use(routes);
 
 app.listen(3333);
