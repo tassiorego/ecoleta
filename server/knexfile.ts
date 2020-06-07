cab4e5ab6492a7fccb55cba9ff39fe0bd8b782a7
Tassio Rego <tassiorego@gmail.com>
2020-06-07 09:25:01 -0300

    creating knexfile

---
+++ server/knexfile.ts
@@ -0,0 +1,11 @@
+const { resolve } = require('path');
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: resolve(__dirname, 'src', 'database', 'database.sqlite'),
+  },
+  migrations: {
+    directory: resolve(__dirname, 'src', 'database', 'migrations'),
+  },
+};
