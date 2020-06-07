a786b20fe3c72cc8b6fa65844057e8f7152e45f1
Tassio Rego <tassiorego@gmail.com>
2020-06-07 12:48:34 -0300

    Adding CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
-import express, { Request, Response } from 'express';
+import cors from 'cors';
+import express from 'express';
 import { resolve } from 'path';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
